835b726c93efb6b39a3ab2917ff817f297ff9848
bru-assis <65248262+bru-assis@users.noreply.github.com>
2020-05-24 15:54:42 -0300

    Upload de imagens

--- postsDB.js
+++ postsDB.js
@@ -1,19 +1,35 @@
 // Salvar o post no DB
-const savePost = document.getElementById('savePost');
-savePost.addEventListener('click', e =>{
+var btnImg = document.getElementById('btnImg');
 
-  const user = firebase.auth().currentUser;
-  const TXTdescricao = document.getElementById('descricao')
-  const TXTtitulo = document.getElementById('titulo');
-e.preventDefault();
+btnImg.addEventListener('change', e =>{
+    const file = e.target.files[0];
+    const filename = file.name;
+    const ref = firebase.storage().ref('/imagens/' + filename);
+    const task = ref.put(file);
+    task.then(snapshot => snapshot.ref.getDownloadURL())
+      .then(url => {
+       var img = document.getElementById('btnImg');
+       img.src = url;
+      })
+      .catch(console.error);
+})
+
+const savePost = document.getElementById('savePost');
+
+savePost.addEventListener('click', e =>{
+    const user = firebase.auth().currentUser;
+    const TXTdescricao = document.getElementById('descricao')
+    const TXTtitulo = document.getElementById('titulo');
+    
+    e.preventDefault();
 
 return firebase.firestore().collection('postagens').add({    
   text: TXTdescricao.value,
   title: TXTtitulo.value,
   timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-  usuario: user.uid
-  }).then(
-      alert('Cadastrado com sucesso!'));
+  usuario: user.uid,
+  imagem: btnImg.src
+  }); 
 });
 
 // Exibir posts
@@ -25,11 +41,14 @@ ref.onSnapshot(snapshot => {
   snapshot.forEach(doc =>  {
       requests.push({...doc.data(), id: doc.id})
   });
-  console.log(requests);
 
   let html = '';
 requests.forEach(request => {
+  html += `<li>`
+  html += `<h5> ${request.title}</h5>`;
   html += `<p> ${request.text}</p>`;
+  html += `<img src= "${request.imagem}" style="width:50px;height:50px;">`;
+  html += `</li>`
 });
 document.getElementById('posts').innerHTML = html;
 });
